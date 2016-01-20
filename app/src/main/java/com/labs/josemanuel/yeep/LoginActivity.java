8509d709307f1b9bda6913e85137d6933ddca718
Javrom95 <javrom95@gmail.com>
2016-01-20 17:04:56 +0100

    Segunda parte, donde comprobamos que los cambos de login y register distinguen entre vacío y no vacío.

--- app/src/main/java/com/labs/josemanuel/yeep/LoginActivity.java
+++ app/src/main/java/com/labs/josemanuel/yeep/LoginActivity.java
@@ -3,18 +3,21 @@ package com.labs.josemanuel.yeep;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     protected TextView mSingUpTextView;
-
+    EditText user, pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        user = (EditText) findViewById(R.id.usernameField);
+        pass = (EditText) findViewById(R.id.passwordField);
         // elimina la barra superior
         getSupportActionBar().hide();
 
@@ -28,4 +31,20 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+
+    public void check(View view){
+
+        if(user.getText().toString().matches("")){
+            Log.i("LoginActivity", "USER EMPTY.");
+        }else{
+            Log.i("LoginActivity","USER NOT EMPTY.");
+        }
+        if(pass.getText().toString().matches("")){
+            Log.i("LoginActivity","PASS EMPTY.");
+        }else{
+            Log.i("LoginActivity","PASS NOT EMPTY.");
+        }
+
+    }
+
 }
--- app/src/main/java/com/labs/josemanuel/yeep/SignUpActivity.java
+++ app/src/main/java/com/labs/josemanuel/yeep/SignUpActivity.java
@@ -2,12 +2,13 @@ package com.labs.josemanuel.yeep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText user, pass;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +22,19 @@ public class SignUpActivity extends AppCompatActivity {
 
     }
 
-    public void
+    public void check(View view){
 
+        if(user.getText().toString().matches("")){
+            Log.i("SignUpActivity","USER EMPTY.");
+        }else{
+            Log.i("SignUpActivity","USER NOT EMPTY.");
+        }
+        if(pass.getText().toString().matches("")){
+            Log.i("SignUpActivity","PASS EMPTY.");
+        }else{
+            Log.i("SignUpActivity","PASS NOT EMPTY.");
+        }
+
+    }
 
 }
