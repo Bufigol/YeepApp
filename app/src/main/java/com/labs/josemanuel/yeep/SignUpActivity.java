ef19fbdd98ac63dc934cadc1823e4941290ae846
Javrom95 <javrom95@gmail.com>
2016-01-20 16:19:42 +0100

    Comienzo de desarrollo funcionalidad de Register.

--- app/src/main/java/com/labs/josemanuel/yeep/SignUpActivity.java
+++ app/src/main/java/com/labs/josemanuel/yeep/SignUpActivity.java
@@ -2,17 +2,26 @@ package com.labs.josemanuel.yeep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText user, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+       user = (EditText) findViewById(R.id.usernameField);
+        pass = (EditText) findViewById(R.id.password_sing);
+
         // elimina la barra superior
         getSupportActionBar().hide();
 
-
     }
+
+    public void
+
+
 }
