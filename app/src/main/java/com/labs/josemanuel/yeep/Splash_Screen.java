93d03fd3caf9245b0db21bef9d1028b5aebf5989
Bufigol <felipevasquez.tort@gmail.com>
2016-01-30 18:52:02 +0100

    Actualización
    Se han actualizado las clases Splash Screen y Login

--- app/src/main/java/com/labs/josemanuel/yeep/Splash_Screen.java
+++ app/src/main/java/com/labs/josemanuel/yeep/Splash_Screen.java
@@ -13,15 +13,27 @@ public class Splash_Screen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        cambiarFont();
+        openApp(true);
+    }
+
+    /**
+     * Metodo Utilizado para establecer un nuevo tipo de letra en el titulo y subtitulo existentes
+     * en el layout del splash screen.
+     */
+    private void cambiarFont() {
         TextView myTitle = (TextView)findViewById(R.id.Title);
         TextView mySubtitle = (TextView)findViewById(R.id.subTitle);
         Typeface myFont = Typeface.createFromAsset(getAssets(), "fonts/Gagalin-Regular.otf");
         myTitle.setTypeface(myFont);
         mySubtitle.setTypeface(myFont);
-        openApp(true);
     }
 
-
+    /**
+      * Metodo que tiene como finalidad de realizar el intent al layout para el inicio de sesión luego
+     * de 2 segundos.
+     * @param locationPermission
+    */
     private void openApp(boolean locationPermission) {
 
         Handler handler = new Handler();
@@ -34,6 +46,6 @@ public class Splash_Screen extends Activity {
                     finish();
                 }
             }, 2000);
-        }
+    }
 }
 
