9d4741cafcfb0e8300ed175f1a4998adbbab51a3
JoseManuel <josemanuelcrv@gmail.com>
2016-02-12 22:11:17 +0100

    Hacer una vídeo mediante un Intent y  Seleccionar un video de la galería: ---------------------------------------------------------------------------------
    Hacer una vídeo mediante un Intent
    *****************************************
    
    Ediciones realizadas:
    
    MainActivity
    -----------------------------
    En el método mDialogListener() :
    se ha creado en la opción 1 un nuevo intent llamado takeVideoIntent que recibe por parámetro en el constructor la constante MediaStore.ACTION_VIDEO_CAPTURE .
    
    Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
    
    se almacena en mMediaUri  la Uri que devuelve el método:
    mMediaUri = FileUtilities.getOutputMediaFileUri(FileUtilities.MEDIA_TYPE_VIDEO);
    
    En el caso de no contener nada  mMediaUri ,  se mostrará un mensaje de  error.
    De lo contrario, añadiremos la siguiente información  extra al Intent con putExtra():
    
    EXTRA_OUTPUT
    EXTRA_DURATION_LIMIT --> establecido el limite de grabación a 10 segundos.
    EXTRA_VIDEO_QUALITY --> establecido calidad del video a la mínima.
    El método  startActivityForResult():
    Recibe por parámetro el intent que acabamos de crear y un número que lo identifica: TAKE_VIDEO_REQUEST
    
    Seleccionar un video de la galería *Edited
    ***************************************
    
    Ediciones realizadas:
    
    MainActivity
    -----------------------------
    
    * Método sizeVideoAdvise:
    Cuando el usuario elige la opción de video desde la galería, al hacer click, llama al método sizeVideoAdvise.
    Lo primero que realiza es  mostrar al usuario una ventana diálogo para informarle sobr el tamaño máximo para los videos.
    Una vez aceptada la advertencia, el méodo realiza las siguientes funciones:
    
    -Crea un nuevo intent para el caso 3 llamado chooseVideoIntent.
         Intent chooseVideoIntent = new Intent(Intent.ACTION_GET_CONTENT);
    
    - Añade un filtro mediante el método setType que recibirá " video/* ".
    
    - Llama al método startActivityForResult , que recibirá por parámetro el intent que acabamos de crear y la constante PICK_VIDEO_REQUEST.
    
    Una vez seleccionado el video, se devolverá el control a MainActivity y se ejecutará el método onActivityResult.
    En el métdo onActivityResult comprobaremos el tamaño del vídeo seleccionado.
    
    - Inicializamos la variable fileSize a 0, la usaremos para almacenar el tamaño del video seleccionado.
    
    - Creamos un objeto InputStream iniciaremos con el método getContentResolver().openInputStream() al que le pasaremos por parámetro el objeto mMediaUri.
    De esta forma tendremos acceso al fichero del vídeo que ha seleccionado el usuario en la galería y podremos obtener su tamaño.
      inputStream = getContentResolver().openInputStream(mMediaUri);
    
    // devuelve el tamaño del video
     fileSize = inputStream.available();
    
    // Comprobación del tamaño del archivo
    fileSize >= FILE_SIZE_LIMIT
    
    Método sizeVideoWarring:
    En caso de ser superior al permitido, el metodo sizeVideoWarring, muestra una ventana dialogo al usuario indicandole que debe seleccionar otro video.

--- app/src/main/java/com/labs/josemanuel/yeep/MainActivity.java
+++ app/src/main/java/com/labs/josemanuel/yeep/MainActivity.java
@@ -9,23 +9,22 @@ import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int TAKE_VIDEO_REQUEST = 1;
     public static final int PICK_PHOTO_REQUEST = 2;
     public static final int PICK_VIDEO_REQUEST = 3;
+    static final int FILE_SIZE_LIMIT = 10485760;
     protected Uri mMediaUri; // permite identificar ficheros
 
     /**
@@ -118,66 +118,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    // creado un array de strings llamado camera_choices en el archivo strings
-
-    // creando metod dialogCameraChoices para la declaracion del dialogo de la opcion camara
-    // se encargara de mostrar el dialogo con las opciones
-    public void dialogCameraChoices() {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setItems(R.array.camera_choices, mDialogListener());
-        AlertDialog dialog = builder.create();
-        dialog.show();
-
-    }
-
-
-    // Creado metodo mDialogListener que implementa variable dialogListener
-    // de tipo DialogInterface.OnClickListener que sobreescribe el metodo onClick.
-    // onClick implementa un switch-case que se encaragara de realizar los distintos
-    // Intents correspondientes a las opciones de la camara
-    private DialogInterface.OnClickListener mDialogListener() {
-
-        DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case 0:
-                        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        //
-                        mMediaUri = FileUtilities.getOutputMediaFileUri(FileUtilities.MEDIA_TYPE_IMAGE);
-                        // Si no existe identificador
-                        if (mMediaUri == null) {
-                            mensajeAlerta();
-                            // Toast.makeText(MainActivity.this, R.string.error_external_storage, Toast.LENGTH_LONG).show();
-                            Log.i(TAG, "Error en el almacenamiento externo");
-                        } else {
-                            // anadiremos informacion extra al intent
-                            takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
-                            startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
-                            Log.i(TAG, "Take Photo Option is selected");
-                        }
-                        break;
-
-                    case 1:
-                        Log.i(TAG, "Take Video Option is selected");
-                        break;
-
-                    case 2:
-                        Log.i(TAG, "Choice Photo Option is selected");
-                        break;
-
-                    case 3:
-                        Log.i(TAG, "Choice Video Option is selected");
-                        break;
-
-                }
-
-            }
-        };
-        return dialogListener;
-    }
-
 
     // modificado el metodo por un bloque switch-case en lugar de bloque if
 
@@ -220,6 +160,93 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    // creado un array de strings llamado camera_choices en el archivo strings
+
+    // creando metod dialogCameraChoices para la declaracion del dialogo de la opcion camara
+    // se encargara de mostrar el dialogo con las opciones
+    public void dialogCameraChoices() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setItems(R.array.camera_choices, mDialogListener());
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+
+    // Creado metodo mDialogListener que implementa variable dialogListener
+    // de tipo DialogInterface.OnClickListener que sobreescribe el metodo onClick.
+    // onClick implementa un switch-case que se encaragara de realizar los distintos
+    // Intents correspondientes a las opciones de la camara
+    private DialogInterface.OnClickListener mDialogListener() {
+
+        DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0: // Take photo
+                        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        //
+                        mMediaUri = FileUtilities.getOutputMediaFileUri(FileUtilities.MEDIA_TYPE_IMAGE);
+                        // Si no existe identificador
+                        if (mMediaUri == null) {
+                            AlertMssgStorage();
+                            // Toast.makeText(MainActivity.this, R.string.error_external_storage, Toast.LENGTH_LONG).show();
+                            Log.i(TAG, "Error en el almacenamiento externo");
+                        } else {
+                            // anadiremos informacion extra al intent
+                            takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                            startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+                            Log.i(TAG, "Take Photo Option is selected");
+                        }
+                        break;
+
+                    case 1: // Take video
+                        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                        mMediaUri = FileUtilities.getOutputMediaFileUri(FileUtilities.MEDIA_TYPE_VIDEO);
+
+
+                        // Si no existe identificador
+                        if (mMediaUri == null) {
+                            AlertMssgStorage();
+                            // Toast.makeText(MainActivity.this, R.string.error_external_storage, Toast.LENGTH_LONG).show();
+                            Log.i(TAG, "Error en el almacenamiento externo");
+                        } else {
+                            // anadiremos informacion extra al intent
+                            takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                            takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10);
+                            takeVideoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
+                            startActivityForResult(takeVideoIntent, TAKE_VIDEO_REQUEST);
+                            Log.i(TAG, "Take Video Option is selected");
+                        }
+
+                        break;
+
+                    case 2: // Choose photo
+                        Intent choosePhotoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                        choosePhotoIntent.setType("image/*");
+                        startActivityForResult(choosePhotoIntent, PICK_PHOTO_REQUEST);
+                        Log.i(TAG, "Choice Photo Option is selected");
+                        break;
+
+                    case 3: // Choose video
+                        sizeVideoAdvise();  // Aviso al usuario sobre el tamaño del video
+                        Log.i(TAG, "Choice Video Option is selected");
+
+                        /*Intent chooseVideoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                        chooseVideoIntent.setType("video/*");
+                        // Toast.makeText(MainActivity.this, R.string.video_file_size_warning, Toast.LENGTH_LONG).show();
+                        startActivityForResult(chooseVideoIntent, PICK_VIDEO_REQUEST);*/
+                        break;
+
+                }
+
+            }
+        };
+        return dialogListener;
+    }
+
+
     // resultado devuelto
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -229,30 +256,69 @@ public class MainActivity extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
             if (requestCode == PICK_PHOTO_REQUEST || requestCode == PICK_VIDEO_REQUEST) {
                 if (data == null) {
-                    mensajeAlerta();
+                    AlertMssgStorage();
                     Toast.makeText(this, getString(R.string.general_error), Toast.LENGTH_LONG).show();
                 } else {
                     mMediaUri = data.getData();
 
                 }
+                // imprimimos por consola la Uri
+                Log.i(TAG, "Media URI: " + mMediaUri);
+
+                // Si se selecciona un video, comprobamos su tamaño  <10Mb
+                if (requestCode == PICK_VIDEO_REQUEST) {
+
+                    int fileSize = 0; // inicializamos variable que almacenará el tamaño del video a 0
+                    InputStream inputStream = null;
+
+                    try {
+                        // acceso al vídeo seleccionado de la galería y obtener su tamaño
+                        inputStream = getContentResolver().openInputStream(mMediaUri);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                    try {
+                        fileSize = inputStream.available(); // devuelve el tamaño del video
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return;
+                    } finally {
+                        try {
+                            inputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    // Comprobación del tamaño del archivo
+                    if (fileSize >= FILE_SIZE_LIMIT) {
+                        sizeVideoWarring();
+                        Toast.makeText(this, "El video es superior a 10MB", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    if (data != null) {
+                        mMediaUri = data.getData();
+
+                    } else {
+                        // Hay algún problema al obtener el video
+                        Toast.makeText(this, "Hay algún problema al obtener el video", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
             }
-
-            // nuevo intent mediaScanIntent iniciado con constante por el constructor
-            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-            mediaScanIntent.setData(mMediaUri); // Anadiremos al intent la uri de la imagen
-            sendBroadcast(mediaScanIntent); // pasaremos por parametro el intent creado
-
         }// FIn RESULT_OK
-        else if (resultCode != RESULT_CANCELED) {
+        else if (resultCode != RESULT_CANCELED) { // si no se devolvió nada y tampoco pulso cancelar
             Toast.makeText(this, getString(R.string.general_error), Toast.LENGTH_LONG).show();
-            mensajeAlerta();
+            AlertMssgStorage();
         }
 
     } // FIn onActivityResult
 
 
-    // metodo para el mensaje de Alerta
-    private void mensajeAlerta() {
+    // Error en el almacenamiento
+    private void AlertMssgStorage() {
 
         final AlertDialog.Builder alertaSimple = new AlertDialog.Builder(MainActivity.this);
         Log.d(TAG, " -*- El popup Dialog se ha creado -*-");
@@ -261,9 +327,52 @@ public class MainActivity extends AppCompatActivity {
         alertaSimple.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                setContentView(R.layout.activity_main);
+                //  setContentView(R.layout.activity_main);
 
             }
+        });
+
+        alertaSimple.setIcon(R.mipmap.ic_launcher);
+        alertaSimple.create();
+        alertaSimple.show();
+    }
+
+    // Aviso al usuario del tamaño máximo para videos
+    private void sizeVideoAdvise() {
+        final AlertDialog.Builder alertaSimple = new AlertDialog.Builder(MainActivity.this);
+        Log.d(TAG, " -*- El popup Dialog se ha creado -*-");
+        alertaSimple.setTitle("Error Storage");
+        alertaSimple.setMessage("El video debe ser inferior a 10Mb");
+        alertaSimple.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Intent chooseVideoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                chooseVideoIntent.setType("video/*");
+                // Toast.makeText(MainActivity.this, R.string.video_file_size_warning, Toast.LENGTH_LONG).show();
+                startActivityForResult(chooseVideoIntent, PICK_VIDEO_REQUEST);
+            }
+
+        });
+
+        alertaSimple.setIcon(R.mipmap.ic_launcher);
+        alertaSimple.create();
+        alertaSimple.show();
+    }
+
+    // ALERT Tamaño de video elegido superado
+    private void sizeVideoWarring() {
+        final AlertDialog.Builder alertaSimple = new AlertDialog.Builder(MainActivity.this);
+        Log.d(TAG, " -*- Mensaje: Tamaño de video excedido -*-");
+        alertaSimple.setTitle("Error");
+        alertaSimple.setMessage(R.string.video_size_warning);
+        alertaSimple.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+            }
+
         });
 
         alertaSimple.setIcon(R.mipmap.ic_launcher);
