ae8ba297da61eb3b379861c639c0fb0249c4ee99
Bufigol <felipevasquez.tort@gmail.com>
2016-02-15 11:44:33 +0100

    solución problema
    @Javrom95 soluciono el problema de que no se mostraba el boton de envio de mensajes cuando se seleccionaba al menos un destinatario.

--- app/src/main/java/com/labs/josemanuel/yeep/Recipients.java
+++ app/src/main/java/com/labs/josemanuel/yeep/Recipients.java
@@ -1,5 +1,4 @@
 package com.labs.josemanuel.yeep;
-
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -8,21 +7,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
-
 import java.util.List;
 
 
-/**
- * Created by JoseManuel on 28/01/2016.
- */
 public class Recipients extends ListActivity {
     public static final String TAG = Recipients.class.getSimpleName();
     protected List<ParseUser> mFriends;
@@ -33,42 +29,35 @@ public class Recipients extends ListActivity {
     protected int[] images;
     public String[] emails;
     public String email;
-
-
+    public ImageButton btnsend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipients);
+        btnsend = (ImageButton) findViewById(R.id.sendbutton);
+        btnsend.setVisibility(View.INVISIBLE);
         // iniciamos el progressBar Spinner
         //pgrsBar = (ProgressBar) findViewById(R.id.progressBar2);
         // activar los checks Y permitir la selección múltiple de registros
         getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-
     }
-
-
     @Override
     public void onResume() {
         super.onResume();
         mCurrentUser = ParseUser.getCurrentUser();
         mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
-
         this.setProgressBarIndeterminateVisibility(true);
         ParseQuery<ParseUser> query = mFriendsRelation.getQuery();
         query.addAscendingOrder(ParseConstants.KEY_USERNAME);
-
         query.findInBackground(new FindCallback<ParseUser>() {
             @Override
             public void done(List<ParseUser> friends, ParseException e) {
-
-
                 if (e == null) {
                     mFriends = friends;
                     usernames = new String[mFriends.size()];
                     emails = new String[usernames.length];
                     Log.i("FriendsFragment.", "Just finishing to set the size of the email array, which it will be: " + emails.length);
                     int i = 0;
-
                     for (ParseUser user : mFriends) {
                         usernames[i] = user.getUsername();
                         emails[i] = user.getEmail().toLowerCase();
@@ -81,8 +70,6 @@ public class Recipients extends ListActivity {
                             usernames);
                     setListAdapter(adapter);
                     getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-
-
                     if (mFriends.size() > 0) {
                         Log.i("FriendsFragment.", "Not empty array.");
                     } else {
@@ -90,36 +77,32 @@ public class Recipients extends ListActivity {
                     }
                 } else {
                     Log.e(TAG, e.getMessage());
-
                     AlertDialog.Builder builder = new AlertDialog.Builder(Recipients.this);
                     builder.setTitle(R.string.error_message)
                             .setMessage(e.getMessage())
                             .setPositiveButton(android.R.string.ok, null);
-
                     AlertDialog dialog = builder.create();
                     dialog.show();
                 }
-
             }
         });
     }
-
     public boolean onCreateOptionsMenu (Menu menu){
         MenuItem mSendItem;
         getMenuInflater().inflate(R.menu.menu_recipients, menu);
         mSendItem=menu.getItem(0);
         return true;
     }
-
+    public void showAction(View view){
+        Log.i(TAG,"Pushing send friends.");
+    }
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
         if(l.getCheckedItemCount()>0) {
-            setVisible(true);
+            btnsend.setVisibility(View.VISIBLE);
         }else{
-            setVisible(false);
+            btnsend.setVisibility(View.INVISIBLE);
         }
     }
-
-
 }
